1cbb8189b93d5a549306dce2530ed898e69d8d73
ricky7583 <ricky7583@naver.com>
2021-10-22 20:41:16 -0400

    DB URI update for running heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI" : "mongodb://localhost/business_contact"
+    //"URI" : "mongodb://localhost/business_contact"
+    "URI" : "mongodb+srv://ricky7583:PsBmSF4G7BTe1xNz@mongodbserver.cpnvh.mongodb.net/business_contact?retryWrites=true&w=majority"
 }
